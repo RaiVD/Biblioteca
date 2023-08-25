d141eda820310f7c5571991d49156a5868dd6e09
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-08-25 16:02:13 -0300

    Initial commit

---
+++ src/main/kotlin/model/InputUserModel.kt
@@ -0,0 +1,27 @@
+package service
+
+class InputUserService {
+    fun readIntFromUser(mensagem: String): Int {
+        while (true) {
+            print(mensagem)
+            val input = readlnOrNull()
+            try {
+                return input?.toInt() ?: throw NumberFormatException()
+            } catch (e: NumberFormatException) {
+                println("Entrada inválida. Por favor, digite um número inteiro válido.")
+            }
+        }
+    }
+    fun readStringFromUser(mensagem: String): String {
+        while (true) {
+            print(mensagem)
+            val input = readlnOrNull()
+
+            try {
+                return input ?: throw NumberFormatException()
+            } catch (e: NumberFormatException) {
+                println("Entrada inválida. Por favor, digite um número válido.")
+            }
+        }
+    }
+}
